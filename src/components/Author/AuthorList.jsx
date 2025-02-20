55bebb2c069a20b6af61043d748c94ded3be5c6b
Rosa <rozasun61@gmail.com>
2025-02-20 11:03:50 +0700

    Latest Update

--- src/components/Author/AuthorList.jsx
+++ src/components/Author/AuthorList.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Authorlist = ({ BookProduct }) => {
   return (
     <div className="all-Author xl:p-4 w-full">
-      <ul className="xl:flex xl:flex-col xl:justify-start xl:items-start gap-4 w-full h-[410px] scrollbar-thumb-teal-500 scrollbar-thumb-rounded-3xl scrollbar-track-rounded-md scrollbar-track-blue-800 scrollbar-thin overflow-auto">
+      <ul className="xl:flex xl:flex-col xl:justify-start xl:items-start gap-4 w-full h-[410px] overflow-hidden">
         {BookProduct.map((item, index) => (
           <li
             className="w-full xl:border-none xl:px-0 xl:py-0 border px-6 py-4"
