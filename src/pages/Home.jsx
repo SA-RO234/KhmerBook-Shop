48a50014bc3c6a57e5f2bee2e920671ee8e1e731
Rosa <rozasun61@gmail.com>
2025-02-26 08:37:46 +0700

    Home Page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,12 +3,15 @@ import React from "react";
 import Maincontent from "../components/Maincontent";
 import CardList from "../components/CardBook/CardList";
 import Navbar from "../components/navbar";
+import { MenuProvider } from "../context/MenuProvider";
 
 const Home = () => {
   return (
     <div className="xl:w-[90%] w-full xl:m-auto xl:pt-5">
-      <Navbar />
-      <Maincontent />
+      <MenuProvider>
+        <Navbar />
+        <Maincontent />
+      </MenuProvider>
       <CardList />
     </div>
   );
