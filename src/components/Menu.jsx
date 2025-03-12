bdd783af29cd207420ed74b18bcb825aa98e3acf
Sun Rosa <rozasun61@gmail.com>
2025-03-12 11:07:08 +0700

    Menu button

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,13 +1,31 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faBars } from "@fortawesome/free-solid-svg-icons/faBars";
-import React, { useContext} from "react"; 
+import React, { useContext,useEffect } from "react";
 import { MenuCotext } from "../context/MenuProvider";
+import { useRef } from "react";
 
 const Menu = () => {
-  const { Open, setOpen }  = useContext(MenuCotext);
+  const { Open, setOpen } = useContext(MenuCotext);
+  const menuButtonref = useRef(null);
+
+  useEffect(() => {
+    const handleClickOutsize = (event) => {
+      if (
+        menuButtonref.current &&
+        !menuButtonref.current.contains(event.target)
+      ) {
+        setOpen(false);
+      }
+    };
+    document.addEventListener("mousedown", handleClickOutsize);
+    return () => {
+       document.removeEventListener("mousedown",handleClickOutsize);
+    };
+  }, [setOpen]);
+
   return (
     <div className="menu xl:hidden">
-      <button type="button" onClick={()=> setOpen(!Open)}>
+      <button type="button" ref={menuButtonref} onClick={() => setOpen(!Open)}>
         <FontAwesomeIcon
           icon={faBars}
           className="text-[35px]"
