b2bfc0da5fbdd66a00bb063429c89e1a6d75a96a
Sun Rosa <rozasun61@gmail.com>
2025-03-15 18:08:04 +0700

    CardList

--- src/components/CardBook/CardList.jsx
+++ src/components/CardBook/CardList.jsx
@@ -1,7 +1,9 @@
 import React from "react";
-
+import Carditem from "./Carditem";
 const CardList = () => {
-  return <div></div>;
+  return <div className="w-full flex flex-wrap justify-between items-center">
+      <Carditem/>
+  </div>; 
 };
 
 export default CardList;
